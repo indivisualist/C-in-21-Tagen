f6a2ed5aef14e3042d2d555f29ab241e42eca775
Matthias <indivisualist@indivisualist.de>
2013-12-16 17:11:29 +0100

    nested fors

---
+++ Tag6/rechteck.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void rechteck_zeichnen(int, int);
+
+int main(void) {
+	rechteck_zeichnen(8, 35);
+	return 0;
+}
+
+void rechteck_zeichnen(int reihe, int spalte) {
+	int spa;
+	for (; reihe>0; reihe--) {
+		for (spa=spalte; spa>0; spa--)
+			printf("X");
+		printf("\n");
+	}
+}
