78e4c1d43f5694007233ce165adc69986d353c4e
Matthias <indivisualist@indivisualist.de>
2013-12-18 18:28:16 +0100

    example for using a union

---
+++ Tag11/unionexample.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+#define CHARACTER	'C'
+#define INTEGER		'I'
+#define FLOAT		'F'
+
+struct s_generisch {
+	char typ;
+	union u_shared {
+		char	c;
+		int	i;
+		float	f;
+	} shared;
+};
+
+void ausgabe(struct s_generisch generisch);
+
+int main(void) {
+
+	struct s_generisch var;
+
+	var.typ = CHARACTER;
+	var.shared.c = '$';
+	ausgabe(var);
+
+	var.typ = FLOAT;
+	var.shared.f = (float) 12345.67890;
+	ausgabe(var);
+
+	var.typ = 'x';
+	var.shared.i = 111;
+	ausgabe(var);
+
+	return 0;
+
+}
+
+void ausgabe(struct s_generisch generisch) {
+
+	printf("\nDer generische Wert ist ...");
+	
+	switch (generisch.typ) {
+		case CHARACTER :
+			printf("%c\n", generisch.shared.c);
+			break;
+		case INTEGER :
+			printf("%d\n", generisch.shared.i);
+			break;
+		case FLOAT :
+			printf("%f\n", generisch.shared.f);
+			break;
+		default :
+			printf("Typ unbekannt: %c\n", generisch.typ);
+			break;
+	}
+
+}
