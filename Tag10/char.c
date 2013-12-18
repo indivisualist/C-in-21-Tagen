62de971fc2ec99b7c21b6bbc9ed872f8cdddaf4d
Matthias <indivisualist@indivisualist.de>
2013-12-18 12:01:10 +0100

    char intro

---
+++ Tag10/char.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+char c1 = 'a';
+char c2 = 90;
+
+int main(void) {
+	printf("\nAls Zeichen lautet Variable c1: %c", c1);
+	printf("\nAls Zahl lautet Variable c1: %d", c1);
+
+	printf("\nAls Zeichen lautet Variable c2: %c", c2);
+	printf("\nAls Zahl lautet Variable c2: %d", c2);
+
+	return 0;
+}
