5c32d6dd028688dbf485a45c70a41428b6087892
Matthias <indivisualist@indivisualist.de>
2013-12-20 11:38:02 +0100

    moved output in function

--- Tag15/charpointer.c
+++ Tag15/charpointer.c
@@ -1,13 +1,23 @@
 #include <stdio.h>
 
+void strings_ausgeben(char *p[], int n);
+
 main () {
 
 	char *meldung[6] = { "Vor", "vier", "Generationen", "begannen", "unsere", "Vorvaeter" };
 	int count;
 
-	for (count=0; count<6; count++)
-		printf("%s ", meldung[count]);
-	
+	strings_ausgeben(meldung, 6);
+
 	return(0);
+
+}
+
+void strings_ausgeben(char *p[], int n) {
+
+	int count;
+
+	for (count=0; count<n; count++)
+		printf("%s ", p[count]);
 
 }
