2f9903f2c2dc50d71dc3329a5be5460f8231e52a
Matthias <indivisualist@indivisualist.de>
2013-12-20 11:00:20 +0100

    added output of sizes

--- Tag15/multidimensional.c
+++ Tag15/multidimensional.c
@@ -8,6 +8,10 @@ int main() {
 	printf("multi[0] = %lu\n", (unsigned long)multi[0]);
 	printf("&multi[0][0] = %lu\n", (unsigned long)&multi[0][0]);
 
+	printf("Die Groesse von multi = %u\n", sizeof(multi));
+	printf("Die Groesse von multi[0] = %u\n", sizeof(multi[0]));
+	printf("Die Groesse von multi[0][0] = %u\n", sizeof(multi[0][0]));
+
 	return(0);
 
 }
