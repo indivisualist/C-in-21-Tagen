db11149e7df78004d14c25ce483424be9e46f715
Matthias <indivisualist@indivisualist.de>
2013-12-20 10:57:36 +0100

    multidimensional array addresses

---
+++ Tag15/multidimensional.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int multi[2][4];
+
+int main() {
+
+	printf("multi = %lu\n", (unsigned long)multi);
+	printf("multi[0] = %lu\n", (unsigned long)multi[0]);
+	printf("&multi[0][0] = %lu\n", (unsigned long)&multi[0][0]);
+
+	return(0);
+
+}
