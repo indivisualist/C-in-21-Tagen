ddebc19469df8761471fe8c38dd2bd04f208ce2c
Matthias <indivisualist@indivisualist.de>
2013-12-22 13:42:23 +0100

    variable arguments

---
+++ Tag18/average.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+float durchschnitt(int num, ...);
+
+int main() {
+
+	float x;
+
+	x = durchschnitt(10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+	printf("Der erste Durchschnittswert betraegt %f.", x);
+	x = durchschnitt(3, 101, 200, 987, 654, 321);
+	printf("\nDer zweite Durchschnittswert betraegt %f.", x);
+
+	return 0;
+
+}
+
+float durchschnitt(int anz, ...) {
+
+	va_list arg_ptr;
+	int count,
+	    gesamt = 0;
+
+	va_start(arg_ptr, anz);
+
+	for (count = 0; count < anz; count++)
+		gesamt += va_arg(arg_ptr, int);
+
+	va_end(arg_ptr);
+
+	return ((float)gesamt/anz);
+
+}
